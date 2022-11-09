565a41ab8b70614463fa59c0460da30f256fde77
Truji21 <105953776+Truji21@users.noreply.github.com>
2022-11-08 18:42:21 -0700

    Interface de productividad

--- interface_productividad.java
+++ interface_productividad.java
@@ -4,11 +4,12 @@
  * @author Andy Fuentes
  * @author David Trujillo
  * @date 8- 11 - 2022
- * Interfaz para 
+ * Interfaz para productividad (planificación de viajes)
  */
 
 public interface productividad {
     
-    public String viajar(String lugar);
-    
+    public String viajar(String lugar){    
+        return "Su destino es " + lugar + ", el viaje sera de " + (int)(Math.random() * (35-1) + 1) + "km y tomara " + (int)(Math.random() * (200-20) + 20) + " minutos";
+    }
 }
