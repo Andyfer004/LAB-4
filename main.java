1caac090026d2a4a116f9700b9bd09a1f0b3b46e
sharlisbg <sharislizza15@gmail.com>
2022-11-10 16:27:06 -0600

    Cambios

--- main.java
+++ main.java
@@ -7,15 +7,31 @@ public class main {
         radio rc = new radio();
         boolean valor = false;
         ArrayList<Double> listaEmisoras = new ArrayList<Double>();
+
+
+        ArrayList<cancion> playlist1 = new ArrayList<cancion>();
+        ArrayList<cancion> playlist2 = new ArrayList<cancion>();
+        ArrayList<cancion> playlist3 = new ArrayList<cancion>();
+
+        playlist1.add(new cancion("Bonito", "3:00", "Jarabe de Palo", "Pop"));
+        playlist1.add(new cancion("Flip", "3:43", "Glass Animals", "Pop"));
+        playlist1.add(new cancion("Cabinet Man", "4:23", "Lemon Demon", "Synth Pop"));
+
+        playlist2.add(new cancion("Is This What You Can Not Do", "4:46", "Gold Celeste", "Alternativo"));
+        playlist2.add(new cancion("Pink Showers", "2:33", "Deeper", "Alternativo"));
+        playlist2.add(new cancion("Dirty Mouth", "2:54", "Alice Phoebe Lou", "Alternativo"));
+
+        playlist3.add(new cancion("The Pyre", "3:40", "Kronowski", "Heavy Metal"));
+        playlist3.add(new cancion("White Flag", "3:40", "Fall of Envy", "Heavy Metal"));
+        playlist3.add(new cancion("The River Flows", "4:20", "Everture", "Rock Alternativo"));
+        
         Scanner scan = new Scanner(System.in);
         String menu ="--Menu de selección-- \n--------------------- \n1. Encender/Apagar \n2. Cambiar volumen \n3. Modo radio \n4. Modo reproduccion \n5. Modo telefono \n6. Modo productividad \nIngrese el numero de la opcion que desea";
         System.out.println(menu);
         opcion = scan.nextInt();
         scan.nextLine();
 
-        ArrayList<cancion> playlist1 = new ArrayList<cancion>();
-        playlist1.add(new cancion("Bonito", "3:00", "Jarabe de Palo", "Pop"));
-        
+
         switch(opcion){
             case 1:
                 System.out.println("¿Deseas encender o apagar la radio? 1. Encender 2. Apagar");
@@ -26,6 +42,7 @@ public class main {
                 else {
                     valor = false;}
                 rc.estado(valor);
+                break;
                                 
             case 2:
                 if (valor == true){
@@ -39,6 +56,7 @@ public class main {
                         valor1 = false;}
                     rc.estado(valor);
                 }
+                break;
                 
                 
             case 3:
@@ -80,8 +98,35 @@ public class main {
                     rc.cargar(listaEmisoras);
                 }
 
+                break; 
+
 
             case 4:
+                String repromenu ="--Menu de selección-- \n--------------------- \n1. Seleccionar una lista de reproducción \n2. Cambiar Canción \n3. Escuchar canción \nIngrese el numero de la opcion que desea";
+                System.out.println(repromenu);
+                int opc = scan.nextInt();
+                scan.nextLine();
+            /*
+             * Seleccionar lista de reproducción (se tienen configuradas algunas para prueba)
+                o Cambiar canción (tanto para adelante como para atrás)
+                o Escuchar canción
+             */
+
+                if(opc == 1){
+                    System.out.println("¿Qué Playlist deseas escuchar? 1. Pop 2. Alternativo 3. ");
+                    double emisora = scan.nextDouble();
+                    rc.guardar(listaEmisoras, emisora);
+                }
+
+                if(opc == 2){
+
+                }
+                
+                if(opc == 3){
+
+                }
+                
+
                 break;
 
 
@@ -91,11 +136,12 @@ public class main {
                 break;
 
             case 6:
-            if (valor == true){
-                System.out.println("¿A dónde desea viajar?");
-                String lugar = scan.nextLine();
-                rc.viajes(lugar);
-            }
+                if (valor == true){
+                    System.out.println("¿A dónde desea viajar?");
+                    String lugar = scan.nextLine();
+                    rc.viajes(lugar);
+                }
+                break;
     }
 }
 }
