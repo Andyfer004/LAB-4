0aa93e77c66f729365e3a032d0d97b7c2d4f19c0
sharlisbg <108887285+sharlisbg@users.noreply.github.com>
2022-11-09 21:54:12 -0600

    Clase contacto

---
+++ contacto.java
@@ -0,0 +1,69 @@
+/*
+ * @author Sharis Barrios 
+ * @author Andy Fuentes
+ * @author David Trujillo
+ * @date 9- 11 - 2022
+ * Clase para crear objetos tipo contactos
+ */
+
+public class contacto {
+    private String nombre; 
+    private String numero; 
+    
+
+    public contacto() {
+        this.nombre = "";
+        this.numero = "";
+    }
+
+    public contacto(String nombre, String numero) {
+        this.nombre = nombre;
+        this.numero = numero;
+    }
+
+    
+    /** 
+     * @return String
+     */
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    
+    /** 
+     * @param nombre
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    
+    /** 
+     * @return String
+     */
+    public String getNumero() {
+        return this.numero;
+    }
+
+    
+    /** 
+     * @param numero
+     */
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+
+   
+    /** 
+     * @return String
+     */
+    @Override
+    public String toString() {
+        return "{" +
+            " nombre='" + getNombre() + "'" +
+            ", numero='" + getNumero() + "'" +
+            "}";
+    }
+    
+}
